7817fc154c22ac54e7fb489d336d2d33192f9f5f
parkdaechul <eocjf4701@gmail.com>
2023-02-09 22:31:54 +0900

    dom -> html 제어

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict";
 
-console.log("hello");
-console.log('heelo2');
+// 선택자를 통해 html 값을 가져올 수 있음
+const id = document.querySelector("#id"), //# = 태그의 id (ex) <input id= >
+  password = document.querySelector("#password"),
+  loginBtn = document.querySelector("button");
+
+console.log(id, password, loginBtn, "hello");
+
+// 클릭이벤트
+loginBtn.addEventListener("click", login);
+
+function login() {
+  
+  const req = {
+    id: id.value,
+    pw: password.value,
+  };
+  console.log(req); // 태그의 value값 가져올수 있음
+
+  //fetch();
+}
