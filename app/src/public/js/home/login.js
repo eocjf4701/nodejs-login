0bd593a43944958a69746cdcd9d6ea305fce52a7
parkdaechul <eocjf4701@gmail.com>
2023-02-10 22:09:01 +0900

    로그인 요청 FETCH 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -3,20 +3,26 @@
 // 선택자를 통해 html 값을 가져올 수 있음
 const id = document.querySelector("#id"), //# = 태그의 id (ex) <input id= >
   password = document.querySelector("#password"),
-  loginBtn = document.querySelector("button");
+  loginBtn = document.querySelector("#loginButton");
 
-console.log(id, password, loginBtn, "hello");
+//console.log(id, password, loginBtn, "hello");
 
 // 클릭이벤트
 loginBtn.addEventListener("click", login);
 
-function login() {
-  
+function login () {
   const req = {
     id: id.value,
     pw: password.value,
-  };
-  console.log(req); // 태그의 value값 가져올수 있음
-
-  //fetch();
+  }
+  console.log("===>" + JSON.stringify(req)),
+  fetch("login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/jsoin",
+    },
+    body: JSON.stringify(req),
+  });
+  
 }
+
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ let ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.home);
 router.get("/login", ctrl.login);
+//router.post("/login", ctrl.login);
 
 // 외부로 내보내기
 module.exports = router;
